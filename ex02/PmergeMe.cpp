a3c9f4cc76b77e9f4c8e34388ee901cdbc2f4502
Erwann Bansse <ebansse@c3r3p6.42perpignan.fr>
2026-01-06 19:33:23 +0100

    mergeSort mainChain

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -47,3 +47,52 @@ void PmergeMe::sortAndDisplay()
     }
     std::cout << std::endl;
 }
+
+void    makePairs(const std::vector<int> & input, std::vector<int> & mainChain, std::vector<int> & pend, int& straggler)
+{
+    size_t i = 1;
+    for (; i < input.size(); i += 2)
+    {
+        if (input[i] < input[i + 1])
+        {
+            mainChain.push_back(input[i + 1]);
+            pend.push_back(input[i]);
+        }
+        else
+        {
+            mainChain.push_back(input[i]);
+            pend.push_back(input[i + 1]);
+        }
+    }
+    straggler = (i < input.size()) ? input[i] : -1;
+}
+
+std::vector<int> merge(const std::vector<int>& left, const std::vector<int>& right)
+{
+    std::vector<int> result;
+    size_t i = 0; size_t j = 0;
+
+    while (i < left.size() && j < right.size())
+    {
+        if (left[i] < right[j])
+            result.push_back(left[i++]);
+        else
+            result.push_back(right[j++]);
+    }
+
+    while (i < left.size()) result.push_back(left[i++]);
+    while (j < right.size()) result.push_back(right[j++]);
+
+    return (result);
+}
+
+std::vector<int> mergeSort(const std::vector<int>& v)
+{
+    if (v.size() <= 1)
+        return v;
+
+    size_t mid = v.size() / 2;
+    std::vector<int> left(v.begin(), v.begin() + mid);
+    std::vector<int> right(v.begin() + mid, v.end());
+    return (merge(mergeSort(left), mergeSort(right)));
+}
